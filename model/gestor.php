267b7a6289c3f3e53b860936630c160104e1ceda
Junior Sanchez <r.a.s.g65@gmail.com>
2019-08-16 00:37:55 -0400

    add buscardor on view dashboard getsor

--- model/gestor.php
+++ model/gestor.php
@@ -25,6 +25,42 @@ class GestorModel extends Model
             WHERE codigo = {$codigo}";
         return $this->execute_query($query);
     }
+
+    public function getDataSearchGestorByDocumentDeudor($documento)
+    {   
+        $table = static::$table;
+        $query = "SELECT ficha.id, gestor.codigo, deudor.tipodocumento, deudor.documento, deudor.nombre, deudor.apellido, acreedor.razon_social FROM {$table}
+            LEFT JOIN cartera_deudor_codeudor as car
+            ON car.id_gestor=gestor.id
+            LEFT JOIN deudor 
+            ON car.id_deudor=deudor.id
+            LEFT JOIN cartera
+            ON cartera.id=car.id_cartera
+            LEFT JOIN acreedor
+            ON acreedor.id=cartera.id_acreedor
+            LEFT JOIN ficha
+            ON ficha.id_cartera=cartera.id and ficha.id_deudor=deudor.id
+            WHERE deudor.documento LIKE '{$documento}%' OR deudor.nombre LIKE '{$documento}%'";
+        return $this->execute_query($query);
+    }
+
+    public function getDataSearchGestorByNameAcreedor($name)
+    {   
+        $table = static::$table;
+        $query = "SELECT ficha.id, gestor.codigo, deudor.tipodocumento, deudor.documento, deudor.nombre, deudor.apellido, acreedor.razon_social FROM {$table}
+            LEFT JOIN cartera_deudor_codeudor as car
+            ON car.id_gestor=gestor.id
+            LEFT JOIN deudor 
+            ON car.id_deudor=deudor.id
+            LEFT JOIN cartera
+            ON cartera.id=car.id_cartera
+            LEFT JOIN acreedor
+            ON acreedor.id=cartera.id_acreedor
+            LEFT JOIN ficha
+            ON ficha.id_cartera=cartera.id and ficha.id_deudor=deudor.id
+            WHERE acreedor.razon_social LIKE '{$name}%'";
+        return $this->execute_query($query);
+    }
 }
 
 
