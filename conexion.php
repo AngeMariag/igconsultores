1dde854ff7ed292463ca52e7783ef41b3f7319fe
Junior Sanchez <r.a.s.g65@gmail.com>
2019-08-25 17:34:23 -0400

    add registro cartera

--- conexion.php
+++ conexion.php
@@ -1,14 +1,12 @@
-<?php 
-	// setlocale(LC_TIME, 'es_VE'); # Localiza en español es_Venezuela
-	// date_default_timezone_set('America/Caracas');
-	$fecha = date("Y-m-d");
-	$hora = date("g:i:s");
-	
-	$host="localhost";
-	$user ="root";
-	$pass = "";
-	$bd = "igconsultores";
-	
-	$cn = new mysqli($host,$user,$pass,$bd) or die("error ".mysqli_error($cn));
-	
-?>
+<?php
+// setlocale(LC_TIME, 'es_VE'); # Localiza en español es_Venezuela
+// date_default_timezone_set('America/Caracas');
+$fecha = date("Y-m-d");
+$hora = date("g:i:s");
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$bd = "igconsultores";
+
+$cn = new mysqli($host, $user, $pass, $bd) or die("error " . mysqli_error($cn));
