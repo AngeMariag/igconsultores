f5b8c822b9a4cc035f394cd09d91e52993216ac0
Angelica Gutierrez <angelikgutierrez1@gmail.com>
2019-08-31 00:54:36 -0500

    funcion format

--- controlador/cartera/modal/addficha.php
+++ controlador/cartera/modal/addficha.php
@@ -520,5 +520,17 @@
         var parent = $(this).parents().get(1);
         $(parent).remove();
     });
-
+    function format(input)
+{
+var num = input.value.replace(/\./g,'');
+if(!isNaN(num)){
+num = num.toString().split('').reverse().join('').replace(/(?=\d*\.?)(\d{3})/g,'$1.');
+num = num.split('').reverse().join('').replace(/^[\.]/,'');
+input.value = num;
+}
+  
+else{ alert('TIPEA SOLO NÚMEROS');
+input.value = input.value.replace(/[^\d\.]*/g,'');
+}
+}
 </script> 
